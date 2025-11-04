01da505ff2fdc78580aad4257cba4fbc0aa03bfa
John Noel MacPhie <jmacphi2@uwo.ca>
2025-11-04 12:34:35 -0500

    Fix main

--- main.py
+++ main.py
@@ -1,61 +1,55 @@
-import numpy as np
-from game import Board
-from game import IllegalAction, GameOver
-from agent import nTupleNewrok
+from nTupleAgent import nTupleNetwork
+from game import Board, IllegalAction, GameOver
 import pickle
-
 from collections import namedtuple
+import numpy as np
 
-"""
-Vocabulary
---------------
-
-Transition: A Transition shows how a board transfromed from a state to the next state. It contains the board state (s), the action performed (a), 
-the reward received by performing the action (r), the board's "after state" after applying the action (s_after), and the board's "next state" (s_next) after adding a random tile to the "after state".
-
-Gameplay: A series of transitions on the board (transition_history). Also reports the total reward of playing the game (game_reward) and the maximum tile reached (max_tile).
-"""
 Transition = namedtuple("Transition", "s, a, r, s_after, s_next")
 Gameplay = namedtuple("Gameplay", "transition_history game_reward max_tile")
 
 
-def play(agent, board, spawn_random_tile=False):
-    "Return a gameplay of playing the given (board) until terminal states."
+def game(board, agent):
     b = Board(board)
-    r_game = 0
-    a_cnt = 0
-    transition_history = []
+    game_r = 0
+    num_a = 0
+    history = []
     while True:
-        a_best = agent.best_action(b.board)
-        s = b.copyboard()
+        s = b.copy_board()
         try:
-            r = b.act(a_best)
-            r_game += r
-            s_after = b.copyboard()
-            b.spawn_tile(random_tile=spawn_random_tile)
-            s_next = b.copyboard()
-        except (IllegalAction, GameOver) as e:
+            a_best = agent.best_action(s)
+            r = b.move(a_best)
+            game_r += r
+            s_after = b.copy_board()
+            b.spawn_tile()
+            s_next = b.copy_board()
+        except GameOver as e:
             # game ends when agent makes illegal moves or board is full
             r = None
             s_after = None
             s_next = None
             break
+        except IllegalAction as e:
+            # game ends when agent makes illegal moves or board is full
+            r = None
+            s_after = None
+            s_next = None
+            break
+
         finally:
-            a_cnt += 1
-            transition_history.append(
+            num_a += 1
+            history.append(
                 Transition(s=s, a=a_best, r=r, s_after=s_after, s_next=s_next)
             )
     gp = Gameplay(
-        transition_history=transition_history,
-        game_reward=r_game,
-        max_tile=2 ** max(b.board),
+        transition_history=history,
+        game_reward=game_r,
+        max_tile=2 ** np.max(b.board),
     )
     learn_from_gameplay(agent, gp)
     return gp
 
 
 def learn_from_gameplay(agent, gp, alpha=0.1):
-    "Learn transitions in reverse order except the terminal transition"
     for tr in gp.transition_history[:-1][::-1]:
         agent.learn(tr.s, tr.a, tr.r, tr.s_after, tr.s_next, alpha=alpha)
 
@@ -63,34 +57,30 @@ def learn_from_gameplay(agent, gp, alpha=0.1):
 def load_agent(path):
     return pickle.load(path.open("rb"))
 
-
-# map board state to LUT
 TUPLES = [
-    # horizontal 4-tuples
-    [0, 1, 2, 3],
-    [4, 5, 6, 7],
-    [8, 9, 10, 11],
-    [12, 13, 14, 15],
-    # vertical 4-tuples
-    [0, 4, 8, 12],
-    [1, 5, 9, 13],
-    [2, 6, 10, 14],
-    [3, 7, 11, 15],
-    # all 4-tile squares
-    [0, 1, 4, 5],
-    [4, 5, 8, 9],
-    [8, 9, 12, 13],
-    [1, 2, 5, 6],
-    [5, 6, 9, 10],
-    [9, 10, 13, 14],
-    [2, 3, 6, 7],
-    [6, 7, 10, 11],
-    [10, 11, 14, 15],
+    # 4 horz
+    [(0, 0), (0, 1), (0, 2), (0, 3)],
+    [(1, 0), (1, 1), (1, 2), (1, 3)],
+    [(2, 0), (2, 1), (2, 2), (2, 3)],
+    [(3, 0), (3, 1), (3, 2), (3, 3)],
+    # 4 vert
+    [(0, 0), (1, 0), (2, 0), (3, 0)],
+    [(0, 1), (1, 1), (2, 1), (3, 1)],
+    [(0, 2), (1, 2), (2, 2), (3, 2)],
+    [(0, 3), (1, 3), (2, 3), (3, 3)],
+    # 9 box
+    [(0, 0), (0, 1), (1, 0), (1, 1)],
+    [(0, 2), (0, 3), (1, 2), (1, 3)],
+    [(2, 0), (2, 1), (3, 0), (3, 1)],
+    [(2, 2), (2, 3), (3, 2), (3, 3)],
+    [(1, 0), (1, 1), (2, 0), (2, 1)],
+    [(1, 2), (1, 3), (2, 2), (2, 3)],
+    [(0, 1), (0, 2), (1, 1), (1, 2)],
+    [(1, 1), (1, 2), (2, 1), (2, 2)],
+    [(2, 2), (2, 3), (3, 1), (3, 2)],
 ]
 
 if __name__ == "__main__":
-    import numpy as np
-
     agent = None
     # prompt to load saved agents
     from pathlib import Path
@@ -111,8 +101,7 @@ if __name__ == "__main__":
     if agent is None:
         print("initialize agent")
         n_games = 0
-        agent = nTupleNewrok(TUPLES)
-
+        agent = nTupleNetwork(TUPLES)
     n_session = 5000
     n_episode = 100
     print("training")
@@ -120,9 +109,11 @@ if __name__ == "__main__":
         for i_se in range(n_session):
             gameplays = []
             for i_ep in range(n_episode):
-                gp = play(agent, None, spawn_random_tile=True)
+                gp = game(None, agent)
                 gameplays.append(gp)
                 n_games += 1
+
+
             n2048 = sum([1 for gp in gameplays if gp.max_tile == 2048])
             mean_maxtile = np.mean([gp.max_tile for gp in gameplays])
             maxtile = max([gp.max_tile for gp in gameplays])
@@ -140,3 +131,4 @@ if __name__ == "__main__":
             fout = "tmp/{}_{}games.pkl".format(agent.__class__.__name__, n_games)
             pickle.dump((n_games, agent), open(fout, "wb"))
             print("agent saved to", fout)
+
